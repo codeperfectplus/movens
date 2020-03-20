1bdba4129d0779eb69491ecac3c3655d1295df47
Deepak Raj <54245038+perfect104@users.noreply.github.com>
2020-03-20 22:06:12 +0530

    update

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ import shutil
 Warning:
     1.Change Your Root Here
 '''
-root = 'C:\\Users\\Flash\\Pictures\\Screenshots\\'
+root = 'C:\\Users\\Flash\\Downloads\\'
 
 paths = [f'{root}Moved Python',
         f'{root}Moved Picture',
